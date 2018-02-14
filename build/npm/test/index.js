25fbb5ad397ef67f263befd855e0c140e4f2bfc6
Daniel Yoder <dyoder@adele.local>
2018-02-13 18:55:17 -0800

    a few more tests for the router

--- build/npm/test/index.js
+++ build/npm/test/index.js
@@ -12,130 +12,133 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var Router, destructure, parse, testDestructure, testTemplate;
+var Router, destructure, parse;
 
 ({ parse, destructure, Router } = require("../src/index"));
 
-testTemplate = function (template, target) {
-  return (0, _amen.test)(template, function () {
-    var _rec = new _powerAssertRecorder(),
-        _rec2 = new _powerAssertRecorder();
-
-    return _powerAssert2.default.deepEqual(_rec._expr(_rec._capt(target, "arguments/0"), {
-      content: "assert.deepEqual(target, parse(template))",
-      filepath: "index.coffee",
-      line: 8
-    }), _rec2._expr(_rec2._capt(parse(_rec2._capt(template, "arguments/1/arguments/0")), "arguments/1"), {
-      content: "assert.deepEqual(target, parse(template))",
-      filepath: "index.coffee",
-      line: 8
-    }));
-  });
-};
-
-testDestructure = function (template, url, target) {
-  return (0, _amen.test)(`${template} : ${url}`, function () {
-    var _rec3 = new _powerAssertRecorder(),
-        _rec4 = new _powerAssertRecorder();
-
-    var f;
-    f = destructure(parse(template));
-    return _powerAssert2.default.deepEqual(_rec3._expr(_rec3._capt(f(_rec3._capt(url, "arguments/0/arguments/0")), "arguments/0"), {
-      content: "assert.deepEqual(f(url), target)",
-      filepath: "index.coffee",
-      line: 13
-    }), _rec4._expr(_rec4._capt(target, "arguments/1"), {
-      content: "assert.deepEqual(f(url), target)",
-      filepath: "index.coffee",
-      line: 13
-    }));
-  });
-};
-
 _asyncToGenerator(function* () {
-  // ((destructure parse "{/foo,bar}{?baz*}") "/abc/def?g=123&h=456")
-  return (0, _amen.print)((yield (0, _amen.test)("Panda Router", [(0, _amen.test)("Template Parser", [testTemplate("/foo/bar?baz=42", ["/foo/bar?baz=42"]), testTemplate("/{foo}/bar?baz=42", ["/", {
-    variables: [{
-      name: "foo"
-    }]
-  }, "/bar?baz=42"]), testTemplate("/foo{/bar}?baz=42", ["/foo", {
-    operator: "/",
-    variables: [{
-      name: "bar"
-    }]
-  }, "?baz=42"]), testTemplate("/foo{/bar*}?baz=42", ["/foo", {
-    operator: "/",
-    variables: [{
-      name: "bar",
-      modifier: "*"
-    }]
-  }, "?baz=42"]), testTemplate("{/foo,bar}?baz=42", [{
-    operator: "/",
-    variables: [{
-      name: "foo"
-    }, {
-      name: "bar"
-    }]
-  }, "?baz=42"]), testTemplate("{/foo,bar*}?baz=42", [{
-    operator: "/",
-    variables: [{
-      name: "foo"
-    }, {
-      name: "bar",
-      modifier: "*"
-    }]
-  }, "?baz=42"]), testTemplate("/foo/bar?baz={baz}", ["/foo/bar?baz=", {
-    variables: [{
-      name: "baz"
-    }]
-  }]), testTemplate("/foo/bar{?baz}", ["/foo/bar", {
-    operator: "?",
-    variables: [{
-      name: "baz"
-    }]
-  }]), testTemplate("/foo/bar{?baz*}", ["/foo/bar", {
-    operator: "?",
-    variables: [{
-      name: "baz",
-      modifier: "*"
-    }]
-  }])]), (0, _amen.test)("Destructuring", [testDestructure("/{foo}", "/abc", {
-    foo: "abc"
-  }), testDestructure("{/foo}", "/abc", {
-    foo: "abc"
-  }), testDestructure("/foo{?baz}", "/foo?baz=123", {
-    baz: "123"
-  }), testDestructure("{/foo,bar}", "/abc/def", {
-    foo: "abc",
-    bar: "def"
-  }), testDestructure("{/foo*}", "/abc/def", {
-    foo: ["abc", "def"]
-  }), testDestructure("/foo{?bar,baz}", "/foo?bar=123&baz=456", {
-    bar: "123",
-    baz: "456"
-  }), testDestructure("{/foo,bar}{?baz}", "/abc/def?baz=123", {
-    foo: "abc",
-    bar: "def",
-    baz: "123"
-  }), testDestructure("{/foo,bar}{?g,h}", "/abc/def?g=123&h=456", {
-    foo: "abc",
-    bar: "def",
-    g: "123",
-    h: "456"
-  }), testDestructure("{/foo,bar}{?baz*}", "/abc/def?g=123&h=456", {
-    foo: "abc",
-    bar: "def",
-    baz: {
+  return (0, _amen.print)((yield (0, _amen.test)("Panda Router", [(0, _amen.test)("Template Parser", function () {
+    var $pass;
+    $pass = function (template, expected) {
+      return (0, _amen.test)(template, function () {
+        var _rec = new _powerAssertRecorder(),
+            _rec2 = new _powerAssertRecorder();
+
+        return _powerAssert2.default.deepEqual(_rec._expr(_rec._capt(expected, "arguments/0"), {
+          content: "assert.deepEqual(expected, parse(template))",
+          filepath: "index.coffee",
+          line: 14
+        }), _rec2._expr(_rec2._capt(parse(_rec2._capt(template, "arguments/1/arguments/0")), "arguments/1"), {
+          content: "assert.deepEqual(expected, parse(template))",
+          filepath: "index.coffee",
+          line: 14
+        }));
+      });
+    };
+    return [$pass("/foo/bar?baz=42", ["/foo/bar?baz=42"]), $pass("/{foo}/bar?baz=42", ["/", {
+      variables: [{
+        name: "foo"
+      }]
+    }, "/bar?baz=42"]), $pass("/foo{/bar}?baz=42", ["/foo", {
+      operator: "/",
+      variables: [{
+        name: "bar"
+      }]
+    }, "?baz=42"]), $pass("/foo{/bar*}?baz=42", ["/foo", {
+      operator: "/",
+      variables: [{
+        name: "bar",
+        modifier: "*"
+      }]
+    }, "?baz=42"]), $pass("{/foo,bar}?baz=42", [{
+      operator: "/",
+      variables: [{
+        name: "foo"
+      }, {
+        name: "bar"
+      }]
+    }, "?baz=42"]), $pass("{/foo,bar*}?baz=42", [{
+      operator: "/",
+      variables: [{
+        name: "foo"
+      }, {
+        name: "bar",
+        modifier: "*"
+      }]
+    }, "?baz=42"]), $pass("/foo/bar?baz={baz}", ["/foo/bar?baz=", {
+      variables: [{
+        name: "baz"
+      }]
+    }]), $pass("/foo/bar{?baz}", ["/foo/bar", {
+      operator: "?",
+      variables: [{
+        name: "baz"
+      }]
+    }]), $pass("/foo/bar{?baz*}", ["/foo/bar", {
+      operator: "?",
+      variables: [{
+        name: "baz",
+        modifier: "*"
+      }]
+    }])];
+  }()), (0, _amen.test)("Destructuring", function () {
+    var $pass;
+    $pass = function (template, url, expected) {
+      return (0, _amen.test)(`${template} : ${url}`, function () {
+        var _rec3 = new _powerAssertRecorder(),
+            _rec4 = new _powerAssertRecorder();
+
+        var f;
+        f = destructure(parse(template));
+        return _powerAssert2.default.deepEqual(_rec3._expr(_rec3._capt(f(_rec3._capt(url, "arguments/0/arguments/0")), "arguments/0"), {
+          content: "assert.deepEqual(f(url), expected)",
+          filepath: "index.coffee",
+          line: 85
+        }), _rec4._expr(_rec4._capt(expected, "arguments/1"), {
+          content: "assert.deepEqual(f(url), expected)",
+          filepath: "index.coffee",
+          line: 85
+        }));
+      });
+    };
+    return [$pass("/{foo}", "/abc", {
+      foo: "abc"
+    }), $pass("{/foo}", "/abc", {
+      foo: "abc"
+    }), $pass("/foo{?baz}", "/foo?baz=123", {
+      baz: "123"
+    }), $pass("{/foo,bar}", "/abc/def", {
+      foo: "abc",
+      bar: "def"
+    }), $pass("{/foo*}", "/abc/def", {
+      foo: ["abc", "def"]
+    }), $pass("/foo{?bar,baz}", "/foo?bar=123&baz=456", {
+      bar: "123",
+      baz: "456"
+    }), $pass("{/foo,bar}{?baz}", "/abc/def?baz=123", {
+      foo: "abc",
+      bar: "def",
+      baz: "123"
+    }), $pass("{/foo,bar}{?g,h}", "/abc/def?g=123&h=456", {
+      foo: "abc",
+      bar: "def",
       g: "123",
       h: "456"
-    }
-  })]), (0, _amen.test)("Router", [(0, _amen.test)("basic test", function () {
-    var _rec5 = new _powerAssertRecorder(),
-        _rec6 = new _powerAssertRecorder(),
-        _rec7 = new _powerAssertRecorder();
-
-    var match, router;
+    }), $pass("{/foo,bar}{?baz*}", "/abc/def?g=123&h=456", {
+      foo: "abc",
+      bar: "def",
+      baz: {
+        g: "123",
+        h: "456"
+      }
+    })];
+  }()), (0, _amen.test)("Router", function () {
+    var $fail, $pass, router;
     router = Router.create();
+    router.add({
+      template: "/foo/bar",
+      data: void 0
+    });
     router.add({
       template: "/foo{/bar}",
       data: "A"
@@ -144,26 +147,72 @@ _asyncToGenerator(function* () {
       template: "{/foo,bar}",
       data: "B"
     });
-    match = router.match("/abc/def");
-    _powerAssert2.default.equal(true, _rec5._expr(_rec5._capt(_rec5._capt(match, "arguments/1/left") != null, "arguments/1"), {
-      content: "assert.equal(true, match != null)",
-      filepath: "index.coffee",
-      line: 135
-    }));
-    return _powerAssert2.default.deepEqual(_rec6._expr(_rec6._capt(match, "arguments/0"), {
-      content: "assert.deepEqual(match, { data: \"B\", bindings: { foo: \"abc\", bar: \"def\" } })",
-      filepath: "index.coffee",
-      line: 136
-    }), _rec7._expr(_rec7._capt({
+    router.add({
+      template: "/abc/def{?baz}",
+      data: "C"
+    });
+    router.add({
+      template: "{/foo,bar}{?baz*}",
+      data: "D"
+    });
+    $pass = function (url, expected) {
+      return (0, _amen.test)(url, function () {
+        var _rec5 = new _powerAssertRecorder(),
+            _rec6 = new _powerAssertRecorder(),
+            _rec7 = new _powerAssertRecorder();
+
+        var match;
+        match = router.match(url);
+        _powerAssert2.default.equal(true, _rec5._expr(_rec5._capt(_rec5._capt(match, "arguments/1/left") != null, "arguments/1"), {
+          content: "assert.equal(true, match != null)",
+          filepath: "index.coffee",
+          line: 151
+        }));
+        return _powerAssert2.default.deepEqual(_rec6._expr(_rec6._capt(match, "arguments/0"), {
+          content: "assert.deepEqual(match, expected)",
+          filepath: "index.coffee",
+          line: 152
+        }), _rec7._expr(_rec7._capt(expected, "arguments/1"), {
+          content: "assert.deepEqual(match, expected)",
+          filepath: "index.coffee",
+          line: 152
+        }));
+      });
+    };
+    $fail = function (url) {
+      return (0, _amen.test)(`no match: ${url}`, function () {
+        var _rec8 = new _powerAssertRecorder();
+
+        var match;
+        match = router.match(url);
+        return _powerAssert2.default.equal(false, _rec8._expr(_rec8._capt(_rec8._capt(match, "arguments/1/left") != null, "arguments/1"), {
+          content: "assert.equal(false, match != null)",
+          filepath: "index.coffee",
+          line: 157
+        }));
+      });
+    };
+    return [$pass("/abc/def", {
       data: "B",
-      bindings: _rec7._capt({
+      bindings: {
         foo: "abc",
         bar: "def"
-      }, "arguments/1/properties/1/value")
-    }, "arguments/1"), {
-      content: "assert.deepEqual(match, { data: \"B\", bindings: { foo: \"abc\", bar: \"def\" } })",
-      filepath: "index.coffee",
-      line: 136
-    }));
-  })])])));
+      }
+    }), $pass("/abc/def?baz=123", {
+      data: "C",
+      bindings: {
+        baz: "123"
+      }
+    }), $pass("/abc/def?g=123&h=456", {
+      data: "D",
+      bindings: {
+        foo: "abc",
+        bar: "def",
+        baz: {
+          g: "123",
+          h: "456"
+        }
+      }
+    }), $fail("/abc?def=123")];
+  }())])));
 })();
--- test/index.coffee
+++ test/index.coffee
@@ -3,143 +3,182 @@ import {print, test} from "amen"
 
 {parse, destructure, Router} = require "../src/index"
 
-testTemplate = (template, target) ->
-  test template, ->
-    assert.deepEqual target, parse template
-
-testDestructure = (template, url, target) ->
-  test "#{template} : #{url}", ->
-    f = destructure parse template
-    assert.deepEqual (f url), target
-
-# ((destructure parse "{/foo,bar}{?baz*}") "/abc/def?g=123&h=456")
-
 do ->
 
   print await test "Panda Router", [
 
-    test "Template Parser", [
+    test "Template Parser", do ->
 
-      testTemplate "/foo/bar?baz=42", [ "/foo/bar?baz=42" ]
+      $pass = (template, expected) ->
+        test template, ->
+          assert.deepEqual expected, parse template
 
-      testTemplate "/{foo}/bar?baz=42",
-        [ "/", { variables: [ name: "foo" ] }, "/bar?baz=42" ]
+      [
 
-      testTemplate "/foo{/bar}?baz=42",
-        [
-          "/foo"
-          { operator: "/", variables: [ name: "bar" ] }
-          "?baz=42"
-        ]
+        $pass "/foo/bar?baz=42", [ "/foo/bar?baz=42" ]
 
-      testTemplate "/foo{/bar*}?baz=42",
-        [
-          "/foo"
-          {
-            operator: "/"
-            variables: [ { name: "bar", modifier: "*" } ]
-          }
-          "?baz=42"
-        ]
+        $pass "/{foo}/bar?baz=42",
+          [ "/", { variables: [ name: "foo" ] }, "/bar?baz=42" ]
 
-      testTemplate "{/foo,bar}?baz=42",
-        [
-          {
-            operator: "/"
-            variables: [
-              { name: "foo" }
-              { name: "bar" }
-            ]
-          }
-          "?baz=42"
-        ]
-
-      testTemplate "{/foo,bar*}?baz=42",
-        [
-          {
-            operator: "/"
-            variables: [
-              { name: "foo" }
-              { name: "bar", modifier: "*" }
-            ]
-          }
-          "?baz=42"
-        ]
-
-      testTemplate "/foo/bar?baz={baz}",
-        [ "/foo/bar?baz=", { variables: [ { name: "baz" } ] } ]
-
-      testTemplate "/foo/bar{?baz}",
-        [ "/foo/bar", { operator: "?", variables: [ { name: "baz" } ] } ]
-
-      testTemplate "/foo/bar{?baz*}",
-        [
-          "/foo/bar",
-          {
-            operator: "?"
-            variables: [ { name: "baz", modifier: "*" } ]
-            }
+        $pass "/foo{/bar}?baz=42",
+          [
+            "/foo"
+            { operator: "/", variables: [ name: "bar" ] }
+            "?baz=42"
           ]
-    ]
 
-    test "Destructuring", [
+        $pass "/foo{/bar*}?baz=42",
+          [
+            "/foo"
+            {
+              operator: "/"
+              variables: [ { name: "bar", modifier: "*" } ]
+            }
+            "?baz=42"
+          ]
 
-      testDestructure "/{foo}", "/abc", foo: "abc"
+        $pass "{/foo,bar}?baz=42",
+          [
+            {
+              operator: "/"
+              variables: [
+                { name: "foo" }
+                { name: "bar" }
+              ]
+            }
+            "?baz=42"
+          ]
 
-      testDestructure "{/foo}", "/abc", foo: "abc"
+        $pass "{/foo,bar*}?baz=42",
+          [
+            {
+              operator: "/"
+              variables: [
+                { name: "foo" }
+                { name: "bar", modifier: "*" }
+              ]
+            }
+            "?baz=42"
+          ]
 
-      testDestructure "/foo{?baz}", "/foo?baz=123", baz: "123"
+        $pass "/foo/bar?baz={baz}",
+          [ "/foo/bar?baz=", { variables: [ { name: "baz" } ] } ]
 
-      testDestructure "{/foo,bar}", "/abc/def", foo: "abc", bar: "def"
+        $pass "/foo/bar{?baz}",
+          [ "/foo/bar", { operator: "?", variables: [ { name: "baz" } ] } ]
 
-      testDestructure "{/foo*}", "/abc/def", foo: [ "abc", "def" ]
+        $pass "/foo/bar{?baz*}",
+          [
+            "/foo/bar",
+            {
+              operator: "?"
+              variables: [ { name: "baz", modifier: "*" } ]
+              }
+            ]
+      ]
 
-      testDestructure "/foo{?bar,baz}",
-        "/foo?bar=123&baz=456",
-        bar: "123", baz: "456"
+    test "Destructuring", do ->
 
-      testDestructure "{/foo,bar}{?baz}", "/abc/def?baz=123",
-        foo: "abc"
-        bar: "def"
-        baz: "123"
+      $pass = (template, url, expected) ->
+        test "#{template} : #{url}", ->
+          f = destructure parse template
+          assert.deepEqual (f url), expected
 
-      testDestructure "{/foo,bar}{?g,h}", "/abc/def?g=123&h=456",
-        foo: "abc"
-        bar: "def"
-        g: "123"
-        h: "456"
+      [
 
-      testDestructure "{/foo,bar}{?baz*}", "/abc/def?g=123&h=456",
-        foo: "abc"
-        bar: "def"
-        baz:
+        $pass "/{foo}", "/abc", foo: "abc"
+
+        $pass "{/foo}", "/abc", foo: "abc"
+
+        $pass "/foo{?baz}", "/foo?baz=123", baz: "123"
+
+        $pass "{/foo,bar}", "/abc/def", foo: "abc", bar: "def"
+
+        $pass "{/foo*}", "/abc/def", foo: [ "abc", "def" ]
+
+        $pass "/foo{?bar,baz}",
+          "/foo?bar=123&baz=456",
+          bar: "123", baz: "456"
+
+        $pass "{/foo,bar}{?baz}", "/abc/def?baz=123",
+          foo: "abc"
+          bar: "def"
+          baz: "123"
+
+        $pass "{/foo,bar}{?g,h}", "/abc/def?g=123&h=456",
+          foo: "abc"
+          bar: "def"
           g: "123"
           h: "456"
 
-    ]
+        $pass "{/foo,bar}{?baz*}", "/abc/def?g=123&h=456",
+          foo: "abc"
+          bar: "def"
+          baz:
+            g: "123"
+            h: "456"
 
-    test "Router", [
+      ]
 
-      test "basic test", ->
-        
-        router = Router.create()
+    test "Router", do ->
 
-        router.add
-          template: "/foo{/bar}"
-          data: "A"
+      router = Router.create()
 
-        router.add
-          template: "{/foo,bar}"
-          data: "B"
+      router.add
+        template: "/foo/bar"
+        data: undefined
 
-        match = router.match "/abc/def"
-        assert.equal true, match?
-        assert.deepEqual match,
+      router.add
+        template: "/foo{/bar}"
+        data: "A"
+
+      router.add
+        template: "{/foo,bar}"
+        data: "B"
+
+      router.add
+        template: "/abc/def{?baz}"
+        data: "C"
+
+      router.add
+        template: "{/foo,bar}{?baz*}"
+        data: "D"
+
+      $pass = (url, expected) ->
+
+        test url, ->
+          match = router.match url
+          assert.equal true, match?
+          assert.deepEqual match, expected
+
+      $fail = (url) ->
+        test "no match: #{url}", ->
+          match = router.match url
+          assert.equal false, match?
+
+
+      [
+
+        $pass "/abc/def",
           data: "B"
           bindings:
             foo: "abc"
             bar: "def"
 
+        $pass "/abc/def?baz=123",
+          data: "C"
+          bindings:
+            baz: "123"
 
+        $pass "/abc/def?g=123&h=456",
+          data: "D"
+          bindings:
+            foo: "abc"
+            bar: "def"
+            baz:
+              g: "123"
+              h: "456"
+
+        $fail "/abc?def=123"
     ]
   ]
